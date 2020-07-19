71b81f3156157d37fa99971a722f590df73b2cb2
kooyunmo <mpbb03@snu.ac.kr>
2020-07-19 12:42:26 +0900

    minor refactoring for stylegan generation

--- GANs/stylegan/pretrained_celebahq.py
+++ GANs/stylegan/pretrained_celebahq.py
@@ -23,9 +23,10 @@ from pydrive.auth import GoogleAuth
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--img-num-start', type=int, default=0, help='A start image number which will be used as a file name.')
     parser.add_argument('--num-imgs', type=int, help='The number of images you want to generate')
     parser.add_argument('--result-dir', type=str, help='Directory path to save generated imgs')
-    parser.add_argument('--gpu-num', type=int, help='gpu device number')
+    parser.add_argument('--gpu-num', type=int, default=0, help='gpu device number')
     return parser.parse_args()
 
 def main():
@@ -53,10 +54,9 @@ def main():
     # Print network details.
     Gs.print_layers()
 
-    image_num = args.num_imgs
     rnd = np.random.RandomState(random.randint(1, 1000))
     os.makedirs(args.result_dir, exist_ok=True) # C:/stylegan/result
-    for i in range(image_num):
+    for i in range(args.img_num_start, args.img_num_start + args.num_imgs):
         # Pick latent vector.
         latents = rnd.randn(1, Gs.input_shape[1])
 
@@ -65,7 +65,8 @@ def main():
         images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
 
         # Save image.
-        png_filename = os.path.join(args.result_dir, str(i + 1) + ".png")
+        zero_len = len(str(args.num_imgs)) - len(str(i))
+        png_filename = os.path.join(args.result_dir, '0'*zero_len + str(i) + ".png")
         PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
 
         # f = drive.CreateFile({'title': i + ".png"})
