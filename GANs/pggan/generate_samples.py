792cd0751e0874fdd0c1761d063121a81fa4195e
kooyunmo <mpbb03@snu.ac.kr>
2020-08-01 18:32:02 +0900

    resolve OOM issue when generate PGGAN images

--- GANs/pggan/generate_samples.py
+++ GANs/pggan/generate_samples.py
@@ -17,6 +17,8 @@ def parse_args():
                         help='output directory to save generated images')
     parser.add_argument('--num-gen-imgs', type=int, default=10,
                         help='the number of images to generate')
+    parser.add_argument('--batch-size', type=int, default=10,
+                        help='inference batch size')
     parser.add_argument('--gpu', type=str, default='0',
                         help='gpu id')
     return parser.parse_args()
@@ -32,23 +34,30 @@ def main():
     with open('karras2018iclr-celebahq-1024x1024.pkl', 'rb') as file:
         G, D, Gs = pickle.load(file)
 
-    # Generate latent vectors.
-    latents = np.random.RandomState(random.randint(1, 1000)).randn(args.num_gen_imgs, *Gs.input_shapes[0][1:]) # 1000 random latents
-
-    # Generate dummy labels (not used by the official networks).
-    labels = np.zeros([latents.shape[0]] + Gs.input_shapes[1][1:])
-
     # Run the generator to produce a set of images.
-    images = Gs.run(latents, labels)
+    # images = Gs.run(latents, labels)
+    for idx in range(args.num_gen_imgs):
+        # Generate latent vectors.
+        latents = np.random.RandomState(random.randint(1, 1000)).randn(1, *Gs.input_shapes[0][1:])
+        # Generate dummy labels (not used by the official networks).
+        labels = np.zeros([latents.shape[0]] + Gs.input_shapes[1][1:])
 
+        images = Gs.run(latents, labels)
+        images = np.clip(np.rint((images + 1.0) / 2.0 * 255.0), 0.0, 255.0).astype(np.uint8)
+        images = images.transpose(0, 2, 3, 1)
+        png_filename = os.path.join(args.result_dir, str(idx).zfill(5) + ".png")
+        PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
+
+    '''
     # Convert images to PIL-compatible format.
     images = np.clip(np.rint((images + 1.0) / 2.0 * 255.0), 0.0, 255.0).astype(np.uint8) # [-1,1] => [0,255]
     images = images.transpose(0, 2, 3, 1) # NCHW => NHWC
 
     # Save images as PNG.
     for idx in range(images.shape[0]):
-        png_filename = os.path.join(args.result_dir, str(idx+1) + ".png")
+        png_filename = os.path.join(args.result_dir, str(idx).zfill(5) + ".png")
         PIL.Image.fromarray(images[idx], 'RGB').save(png_filename)
+    '''
 
 if __name__ == '__main__':
     main()
